685ac267d9c43b8c3e74355b570087b82c353f53
Michael F. Weinberg <michael@luckymike.com>
2013-10-22 11:58:35 -0700

    added name

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Installs Leiningen for building Clojure projects."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+name             "leiningen"
 version          "0.3.0"
 
 supports "ubuntu"
