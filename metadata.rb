23d1d031df7d3f5f69afadf4f043c253a1936c8e
Michael F. Weinberg <michael@luckymike.com>
2013-10-22 11:56:31 -0700

    Remove metadata.json, tweak spacing in metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ description      "Installs Leiningen for building Clojure projects."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.3.0"
 
-supports         "ubuntu"
-supports         "debian"
+supports "ubuntu"
+supports "debian"
 
-depends          "java"
+depends "java"
